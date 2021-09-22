38870e9081b902a63336ff9391c3bf4a68a0aab0
rrayatec <79470126+rrayatec@users.noreply.github.com>
2021-09-21 19:39:42 -0500

    hooks/advance

--- src/App.js
+++ src/App.js
@@ -1,331 +1,187 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
+import { Container, Navbar, Nav, Col, NavDropdown, Row } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
-import ToggleButton from "@mui/material/ToggleButton";
-import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import { styled } from "@mui/material/styles";
+import Sandwiches from "./routers/SandwichesRouters";
+import MyOffSet from "./components/MyOffSet";
+import Loading from "./components/Loading";
+import MyVerticallyCenteredModal from "./components/MyVerticallyCenteredModal";
 
-import Box from "@mui/material/Box";
-import MuiDrawer from "@mui/material/Drawer";
-import MuiAppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import CssBaseline from "@mui/material/CssBaseline";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-import DashboardCustomizeTwoToneIcon from "@mui/icons-material/DashboardCustomizeTwoTone";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Skeleton from "@mui/material/Skeleton";
-import Avatar from "@mui/material/Avatar";
-import Login from "./pages/Login";
-import SignInSide from "./pages/LoginFull";
-import SignUp from "./pages/signup";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import Badge from "@mui/material/Badge";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Chart from "./components/Chart";
-import Orders from "./components/Orders";
-import Deposits from "./components/Deposits";
+import MyButton from "./components/MyButton";
+//todo
+import useTodoState from "./functions/useTodoState";
+import TodoForm from "./components/todo/TodoForm";
 
-const theme = createTheme({
-  palette: {
-    meunIcons: {
-      main: "#0288d1",
-    },
-  },
-});
+import Button from "@material-ui/core/Button";
+import Snackbar from "@material-ui/core/Snackbar";
 
-const drawerWidth = 250;
+function App() {
+  const [modalShow, setModalShow] = useState(false);
+  const [btnActivo, setBtnActivo] = useState("Cargando");
+  const [mnsActivo, setMnsActivo] = useState("");
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  const [nombres, setNombres] = useState([]);
 
-const openedMixin = (theme) => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
-
-const closedMixin = (theme) => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 20px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(9)} + px)`,
-  },
-});
-
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open && {
-    ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
-  }),
-  ...(!open && {
-    ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
-  }),
-}));
-
-export default function MiniDrawer() {
   const [open, setOpen] = React.useState(false);
-  const [view, setView] = React.useState("list");
 
-  const handleChange = (event, nextView) => {
-    setView(nextView);
+  //todo
+  const { todos, addTodo, deleteTodo, addSingleTodo } = useTodoState([]);
+
+  const handleSelect = () => {
+    setModalShow(true);
   };
 
-  const handleDrawerOpen = () => {
+  const handleSelectFromButton = (props) => {
+    console.log(props.target.id);
+    setBtnActivo(props.target.id);
+    setModalShow(true);
+  };
+
+  const handleClick = (props) => {
+    setMnsActivo(props);
     setOpen(true);
   };
 
-  const handleDrawerClose = () => {
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
     setOpen(false);
   };
 
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      setNombres(res.data);
+      addTodo([...todos, res.data]);
+    });
+  }, []);
+
   return (
     <Router>
-      <Box
-          component="main"
-          sx={{
-            display:"flex",
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
-          }}
-        >
-        <CssBaseline />
-        <AppBar open={open}>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerOpen}
-              edge="start"
-              sx={{
-                marginRight: "36px",
-                ...(open && { display: "none" }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              Dashboard
-            </Typography>
-            <IconButton color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <Drawer variant="permanent" open={open}>
-          <DrawerHeader>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === "rtl" ? (
-                <ChevronRightIcon />
-              ) : (
-                <ChevronLeftIcon />
-              )}
-            </IconButton>
-          </DrawerHeader>
-          <Divider />
-          <ThemeProvider theme={theme}>
-            <ToggleButtonGroup
-              color="meunIcons"
-              orientation="vertical"
-              value={view}
-              exclusive
-              onChange={handleChange}
-              onClick={handleDrawerClose}
-            >
-              <ToggleButton
-                value="Login"
-                button
-                component={Link}
-                to="/login"
-                key={0}
-              >
-                <ListItem>
-                  <ListItemIcon>
-                    <DashboardCustomizeTwoToneIcon />
-                  </ListItemIcon>
-                  <ListItemText primary={"Login"} />
-                </ListItem>
-              </ToggleButton>
+      <Navbar
+        style={{ paddingLeft: 20, paddingRight: 20 }}
+        sticky="top"
+        collapseOnSelect
+        bg="dark"
+        variant="dark"
+        expand="md"
+      >
+        <LinkContainer to="/tacos">
+          <Navbar.Brand>
+            {" "}
+            <img
+              alt=""
+              src="https://react-bootstrap.github.io/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            SAI
+          </Navbar.Brand>
+        </LinkContainer>
 
-              <ToggleButton
-                value="signup"
-                button
-                component={Link}
-                to="/signup"
-                key={0}
-              >
-                <ListItem>
-                  <ListItemIcon>
-                    <DashboardCustomizeTwoToneIcon />
-                  </ListItemIcon>
-                  <ListItemText primary={"signup"} />
-                </ListItem>
-              </ToggleButton>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <LinkContainer to="/tacos">
+              <Nav.Link>Tacos</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/sandwiches">
+              <Nav.Link>Sandwiches</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/sandwiches">
+              <Nav.Link>{btnActivo}</Nav.Link>
+            </LinkContainer>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item onClick={() => handleShow()}>
+                Action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={() => handleSelect()}>
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
 
-              <ToggleButton
-                value="dashboard"
-                button
-                component={Link}
-                to="/dashboard"
-                key={0}
-              >
-                <ListItem>
-                  <ListItemIcon>
-                    <DashboardCustomizeTwoToneIcon />
-                  </ListItemIcon>
-                  <ListItemText primary={"Dashboard"} />
-                </ListItem>
-              </ToggleButton>
+          <MyButton
+            todos={todos}
+            _handleSelectFromButton={(a) => handleSelectFromButton(a)}
+          />
+        </Navbar.Collapse>
+      </Navbar>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        todos={todos}
+        deleteTodo={deleteTodo}
+        _handleClick={(a) => handleClick(a)}
+      />
+      <MyOffSet
+        show={show}
+        onHide={() => setShow(false)}
+        todos={todos}
+        deleteTodo={deleteTodo}
+        _handleClick={(a) => handleClick(a)}
+      />
 
-              <ToggleButton
-                value="profile"
-                button
-                component={Link}
-                to="/profile"
-                key={1}
-              >
-                <ListItem>
-                  <ListItemIcon>
-                    <AccountCircleOutlinedIcon />
-                  </ListItemIcon>
-                  <ListItemText primary={"Profile"} />
-                </ListItem>
-              </ToggleButton>
-            </ToggleButtonGroup>
-          </ThemeProvider>
-        </Drawer>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message={mnsActivo}
+        action={
+          <React.Fragment>
+            <Button color="secondary" size="small" onClick={handleClose}>
+              Deleted
+            </Button>
+          </React.Fragment>
+        }
+      />
 
-        <Switch>
-          <Route path={"/login"}>
-            <SignInSide />
-          </Route>
-          <Route path={"/signup"}>
-            <SignUp />
-          </Route>
-          <Route path={"/dashboard"}>
-            <Container maxWidth="lg" sx={{ mt: 15 }}>
-              <Grid container spacing={3}>
-                {/* Chart */}
-                <Grid item xs={12} md={8} lg={9}>
-                  <Paper
-                    sx={{
-                      p: 2,
-                      display: "flex",
-                      flexDirection: "column",
-                      height: 240,
-                    }}
-                  >
-                    <Chart />
-                  </Paper>
-                </Grid>
-                {/* Recent Deposits */}
-                <Grid item xs={12} md={4} lg={3}>
-                  <Paper
-                    sx={{
-                      p: 2,
-                      display: "flex",
-                      flexDirection: "column",
-                      height: 240,
-                    }}
-                  >
-                    <Deposits />
-                  </Paper>
-                </Grid>
-                {/* Recent Orders */}
-                <Grid item xs={12}>
-                  <Paper
-                    sx={{ p: 2, display: "flex", flexDirection: "column" }}
-                  >
-                    <Orders />
-                  </Paper>
-                </Grid>
-              </Grid>
+      <Switch>
+        <Route path={"/tacos"}>
+          {todos.length === 0 || todos.length === null ? (
+            <Loading />
+          ) : (
+            <Container style={{ padding: 30 }}>
+              <TodoForm
+                addSingleTodo={(todoText) => {
+                  const trimmedText = todoText.name.trim();
+
+                  if (trimmedText.length > 0) {
+                    addSingleTodo(todoText);
+                  }
+                }}
+              />
             </Container>
-          </Route>
-          <Route path={"/profile"}>
-            <Box sx={{ flexGrow: 1, p: 3 }}>
-              <DrawerHeader />
-              <Box sx={{ display: "flex", alignItems: "center" }}>
-                <Box sx={{ margin: 1 }}>
-                  <Skeleton variant="circular">
-                    <Avatar />
-                  </Skeleton>
-                </Box>
-
-                <Box sx={{ width: "100%" }}>
-                  <Skeleton width="100%">
-                    <Typography>.</Typography>
-                  </Skeleton>
-                </Box>
-              </Box>
-              <Skeleton variant="rectangular" width="100%">
-                <div style={{ paddingTop: "10%" }} />
-              </Skeleton>
-            </Box>
-          </Route>
-        </Switch>
-      </Box>
+          )}
+        </Route>
+        <Route path={"/sandwiches"}>
+          {nombres.length === 0 || nombres.length === null ? (
+            <Loading />
+          ) : (
+            <Container style={{ padding: 20 }}>
+              <Row>
+                <Col></Col>
+                <Col>
+                  <Sandwiches data={nombres} />
+                </Col>
+                <Col></Col>
+              </Row>
+            </Container>
+          )}
+        </Route>
+      </Switch>
     </Router>
   );
 }
+
+export default App;
